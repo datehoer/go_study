24004c6b07ae066209aefb6e26f6075b3d9fb487
Kisger <humingkai@int-yt.com>
2022-09-06 09:30:39 +0800

    add readme

--- study/main/echo.go
+++ study/main/echo.go
@@ -5,9 +5,9 @@ import (
 	"os"
 )
 
-func main()  {
-	var s, sep string	
-	for i := 1; i< len(os.Args); i++ {
+func main() {
+	var s, sep string
+	for i := 1; i < len(os.Args); i++ {
 		s += sep + os.Args[i]
 		sep = " "
 	}
